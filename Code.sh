fd906ce37a37b8badad23c1314d61bb9a575b772
said3427 <said3427@gmail.com>
2020-02-29 02:26:36 -0600

    Adding some comments

--- Code.sh
+++ Code.sh
@@ -1,3 +1,4 @@
+# Said Muñoz Montero - said3427@gmail.com
 # Installation
 ## Install requirements using conda
 conda install -y -c bioconda stringtie==2.1.1
@@ -11,19 +12,24 @@ conda install -y -c bioconda bioconductor-ballgown
 wget http://www.genoscope.cns.fr/externe/ONT_mouse_RNA/data/transcriptome/RNA_nanopore.liver.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam
 wget http://www.genoscope.cns.fr/externe/ONT_mouse_RNA/data/transcriptome/RNA_nanopore.brain.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam
 
+## Result directory
+mkdir results
+
 # Transcript assembly
 ## -R parameter is necessary for long reads
-stringtie -R RNA_nanopore.liver.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam  > liver.gtf
-stringtie -R RNA_nanopore.brain.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam  > brain.gtf
+stringtie -R RNA_nanopore.liver.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam  > results/liver.gtf
+stringtie -R RNA_nanopore.brain.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam  > results/brain.gtf
+
 
 ## 1000 lines of GTF
-head -n 1000 liver.gtf > liver1000lines.gtf
-head -n 1000 brain.gtf > brain1000lines.gtf
+head -n 1000 results/liver.gtf > results/liver1000lines.gtf
+head -n 1000 results/brain.gtf > results/brain1000lines.gtf
 
 # Tablemaker
-./tablemaker-2.1.1.Linux_x86_64/tablemaker -q -W -G liver.gtf RNA_nanopore.brain.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam -o brain
-./tablemaker-2.1.1.Linux_x86_64/tablemaker -q -W -G liver.gtf RNA_nanopore.liver.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam -o liver
+./tablemaker-2.1.1.Linux_x86_64/tablemaker -q -W -G results/liver.gtf RNA_nanopore.brain.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam -o brain
+./tablemaker-2.1.1.Linux_x86_64/tablemaker -q -W -G results/liver.gtf RNA_nanopore.liver.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC.bam -o liver
 
-
+# R script
+## This script load the tablemaker results and extract the first 1000 lines of FPKM
 R CMD BATCH load.r
 
--- load.r
+++ load.r
@@ -1,8 +1,20 @@
+# Said Muñoz Montero - said3427@gmail.com
+## Loading ballgown package
+## I installed the package using conda, the command is in Code.sh script
 library(ballgown)
-bgLiver = ballgown(samples="liver")
-bgBrain = ballgown(samples="brain")
+
+## Load Liver results
+bgLiver <- ballgown(samples="liver")
+## Load Brain results
+bgBrain <- ballgown(samples="brain")
+
+## extract transcript-level expression measurements for Liver and Brain samples
 LiverTranscripts <- texpr(bgLiver)
 BrainTranscripts <- texpr(bgBrain)
+
+## Writing the first 1000 lines in CSV
 write.csv(head(LiverTranscripts,1000),file="LiverTranscripts.csv")
 write.csv(head(BrainTranscripts,1000),file="BrainTranscripts.csv")
+
+## Finally, saving the Rdata object
 save(bgLiver,bgBrain, file='DataBallgown.rda')
